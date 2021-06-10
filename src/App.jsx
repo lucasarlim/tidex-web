4fef320babcb5cc5d8bcde264f045049fb76addf
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-10 00:30:46 -0300

    global styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/global';
 import Routes from './Routes';
 
 function App() {
 	return (
 		<BrowserRouter>
+			<GlobalStyles />
 			<Routes />
 		</BrowserRouter>
 	);
