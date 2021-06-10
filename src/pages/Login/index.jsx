f6a3f5363dfb6cecad031379992d410b4f41bc6d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-10 17:11:14 -0300

    login page html structure created

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,43 @@
-import { Container, Title } from './styles';
+import {
+	Container,
+	Banner,
+	TopRow,
+	Message,
+	City,
+	FormWrapper,
+	Title,
+} from './styles';
+import logo from '../../assets/brand/logo-light.svg';
+import bgCity from '../../assets/images/bg-city.svg';
+import frontCity from '../../assets/images/front-city.svg';
 
 function Login() {
 	return (
 		<Container>
-			<Title>Login</Title>
+			<Banner>
+				<TopRow>
+					<img src={logo} alt="TideX" />
+				</TopRow>
+
+				<Message>
+					<h1>
+						<strong>Um acidente, uma vida.</strong> E se fosse você?
+					</h1>
+					<span>Trânsito seguro: um desafio possível.</span>
+				</Message>
+
+				<City>
+					<img src={frontCity} alt="Cidade" />
+					<img src={bgCity} alt="Fundo da cidade" />
+				</City>
+			</Banner>
+
+			<FormWrapper>
+				<Title>Login</Title>
+
+				<input type="text" />
+				<input type="text" />
+			</FormWrapper>
 		</Container>
 	);
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,9 +1,54 @@
 import styled from 'styled-components';
 import { useTheme } from '../../styles/theme';
 
-export const Container = styled.div``;
-
-export const Title = styled.h1`
-	color: ${useTheme.primary};
-	font-size: 60px;
+export const Container = styled.div`
+	width: 100%;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: row;
 `;
+
+export const Banner = styled.section`
+	flex: 0.5;
+	display: flex;
+	flex-direction: column;
+	background-color: ${useTheme.primary};
+`;
+
+export const TopRow = styled.div`
+	flex: 1;
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: flex-start;
+	justify-content: flex-start;
+	padding: 20px;
+
+	& > img {
+		width: 144px;
+	}
+`;
+
+export const Message = styled.div``;
+
+export const City = styled.div`
+	width: 100%;
+
+	& > img:nth-child(1) {
+		width: 50%;
+		display: block;
+		z-index: 1;
+		position: absolute;
+	}
+
+	& > img:nth-child(2) {
+		width: 100%;
+		display: block;
+	}
+`;
+
+export const FormWrapper = styled.div`
+	flex: 0.5;
+`;
+
+export const Title = styled.h1``;
