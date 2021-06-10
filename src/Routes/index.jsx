b61a623be43acf8e12c3a5667adce3bfe01af460
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-10 00:42:06 -0300

    dashboard route changed to /painel

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,4 +1,5 @@
 import { Redirect, Switch } from 'react-router-dom';
+
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import Route from './Route';
@@ -7,8 +8,8 @@ function Routes() {
 	return (
 		<Switch>
 			<Route exact path="/login" component={Login} />
-			<Route exact path="/dashboard" isPrivate component={Dashboard} />
-			<Route exact path="/*" component={() => <Redirect to="/dashboard" />} />
+			<Route exact path="/painel" isPrivate component={Dashboard} />
+			<Route exact path="/*" component={() => <Redirect to="/painel" />} />
 		</Switch>
 	);
 }
