d80c8219bf5dd0fbbabf78df0ec6f348174c5baa
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-11 02:36:11 -0300

    others buttons modes added

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { Container, Loading, Label } from './styles';
 
-function Button({ label, loading, onClick, type }) {
+function Button({ label, loading, onClick, type, mode }) {
 	return (
-		<Container type={type} onClick={onClick}>
+		<Container mode={mode} type={type} onClick={onClick}>
 			{loading ? <Loading /> : <Label>{label}</Label>}
 		</Container>
 	);
@@ -14,12 +14,14 @@ Button.propTypes = {
 	loading: PropTypes.bool,
 	onClick: PropTypes.func,
 	type: PropTypes.string,
+	mode: PropTypes.oneOf(['default', 'empty', 'cancel', 'next']),
 };
 
 Button.defaultProps = {
 	loading: false,
 	onClick: undefined,
 	type: 'button',
+	mode: 'default',
 };
 
 export default Button;
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,12 +1,51 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { useTheme } from '../../styles/theme';
+
+function getStyleByMode(mode) {
+	switch (mode) {
+		case 'default': {
+			return css`
+				color: ${useTheme.light};
+				background-color: ${useTheme.dark};
+				border: none;
+			`;
+		}
+		case 'empty': {
+			return css`
+				color: ${useTheme.dark};
+				background: none;
+				border: none;
+			`;
+		}
+		case 'cancel': {
+			return css`
+				color: ${useTheme.danger};
+				border: solid 1px ${useTheme.danger};
+				background: none;
+			`;
+		}
+		case 'next': {
+			return css`
+				color: ${useTheme.light};
+				background-color: ${useTheme.success};
+				border: none;
+			`;
+		}
+		default: {
+			return css`
+				color: ${useTheme.light};
+				background-color: ${useTheme.dark};
+				border: none;
+			`;
+		}
+	}
+}
 
 export const Container = styled.button`
 	border-radius: 5px;
 	width: 256px;
 	height: 48px;
-	background-color: ${useTheme.dark};
-	border: none;
+	${(props) => getStyleByMode(props.mode)}
 `;
 
 export const Loading = styled.img``;
@@ -14,5 +53,4 @@ export const Loading = styled.img``;
 export const Label = styled.p`
 	font-size: 24px;
 	font-weight: 700;
-	color: ${useTheme.light};
 `;
