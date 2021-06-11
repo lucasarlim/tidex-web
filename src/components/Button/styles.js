1e29b64679c15e2ab8de9c457886be05131d61f2
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-10 23:39:51 -0300

    error modal style added

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
+import { useTheme } from '../../styles/theme';
 
-export const Container = styled.button``;
+export const Container = styled.button`
+	border-radius: 5px;
+	width: 256px;
+	height: 48px;
+	background-color: ${useTheme.dark};
+	border: none;
+`;
 
 export const Loading = styled.img``;
 
-export const Label = styled.p``;
+export const Label = styled.p`
+	font-size: 24px;
+	font-weight: 700;
+	color: ${useTheme.light};
+`;
