d7dafd09667ba9cc8f0f4f95c744673d49a76e83
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-06-11 02:03:54 -0300

    all routes made public

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -14,23 +14,13 @@ function Routes() {
 	return (
 		<Switch>
 			<Route exact path="/login" component={Login} />
-			<Route exact path="/acidentes" component={Accidents} isPrivate />
-			<Route
-				exact
-				path="/acidentes/registrar"
-				component={RegisterAccident}
-				isPrivate
-			/>
-			<Route
-				exact
-				path="/acidentes/editar"
-				component={EditAccident}
-				isPrivate
-			/>
-			<Route exact path="/usuarios" component={Users} isPrivate />
-			<Route exact path="/usuarios/criar" component={AddUser} isPrivate />
-			<Route exact path="/usuarios/editar" component={EditUser} isPrivate />
-			<Route exact path="/estatisticas" component={Statistics} isPrivate />
+			<Route exact path="/acidentes" component={Accidents} />
+			<Route exact path="/acidentes/registrar" component={RegisterAccident} />
+			<Route exact path="/acidentes/editar" component={EditAccident} />
+			<Route exact path="/usuarios" component={Users} />
+			<Route exact path="/usuarios/criar" component={AddUser} />
+			<Route exact path="/usuarios/editar" component={EditUser} />
+			<Route exact path="/estatisticas" component={Statistics} />
 			<Route exact path="/*" component={() => <Redirect to="/acidentes" />} />
 		</Switch>
 	);
