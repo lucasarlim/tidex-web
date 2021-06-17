7a377845eb81db1e7287cc853ea0367e07e4c102
Adriano Lira <adriano.junior@ccc.ufcg.edu.br>
2021-06-16 23:33:37 -0300

    styled create user and edit user

--- src/pages/AddUser/index.jsx
+++ src/pages/AddUser/index.jsx
@@ -1,9 +1,45 @@
 import Menu from '../../components/Menu';
+import FormHeader from '../../components/FormHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import BtnFooter from '../../components/BtnFooter';
+import { Container, Form, Wrapper, Column } from './styles';
 
 function AddUser() {
 	return (
 		<Menu>
-			<h1>AddUser</h1>
+			<Container>
+				<FormHeader title="Cadastrar Usuário" />
+
+				<Form>
+					<Wrapper>
+						<Column>
+							<Select
+								label="Tipo de Usuário"
+								placeholder="Selecione"
+								isRequired
+							/>
+
+							<Input label="Nome" placeholder="Ex: Matheus" isRequired />
+
+							<Input label="Sobrenome" placeholder="Ex: Silva" />
+						</Column>
+
+						<Column>
+							<Input label="CPF" placeholder="Digite o seu CPF" isRequired />
+
+							<Input label="Senha" placeholder="Digite sua senha" isRequired />
+						</Column>
+					</Wrapper>
+
+					<BtnFooter
+						confirmLabel="Concluir"
+						cancelLabel="Cancelar"
+						onConfirm={() => console.log('concluir')}
+						onCancel={() => console.log('cancelar')}
+					/>
+				</Form>
+			</Container>
 		</Menu>
 	);
 }
--- src/pages/EditUser/index.jsx
+++ src/pages/EditUser/index.jsx
@@ -1,11 +1,48 @@
 import Menu from '../../components/Menu';
+import FormHeader from '../../components/FormHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import BtnFooter from '../../components/BtnFooter';
+import { Container, Form, Wrapper, Column } from './styles';
 
-function Accidents() {
+function EditUser() {
 	return (
 		<Menu>
-			<h1>Accidents</h1>
+			<Container>
+				<FormHeader title="Editar Usuário" />
+
+				<Form>
+					<Wrapper>
+						<Column>
+							<Select label="Tipo de Usuário" placeholder="Admin" isRequired />
+
+							<Input label="Nome" placeholder="Carlos" isRequired />
+
+							<Input label="Sobrenome" placeholder="Arthur" />
+						</Column>
+
+						<Column>
+							<Input label="CPF" placeholder="765.323.555-12" isRequired />
+
+							<Input
+								label="Senha"
+								placeholder="*********"
+								password
+								isRequired
+							/>
+						</Column>
+					</Wrapper>
+
+					<BtnFooter
+						confirmLabel="Concluir"
+						cancelLabel="Cancelar"
+						onConfirm={() => console.log('concluir')}
+						onCancel={() => console.log('cancelar')}
+					/>
+				</Form>
+			</Container>
 		</Menu>
 	);
 }
 
-export default Accidents;
+export default EditUser;
