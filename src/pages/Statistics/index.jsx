87666d327d70695acbcb2782665e8409fe86efb3
Lucas Araújo de Lima <70279700+lucasarlim@users.noreply.github.com>
2021-06-17 13:10:16 -0300

    Added Spinner in Statistics

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -18,6 +18,7 @@ import {
 } from 'recharts';
 import { useApiHandler } from '../../contexts/ApiHandlerContext';
 import Menu from '../../components/Menu';
+import loader from '../../assets/icons/loader.svg';
 import { Container, Title, ChartWrapper, ChartTitle } from './styles';
 
 function Statistics() {
@@ -66,7 +67,10 @@ function Statistics() {
 					<ChartTitle>Quantidade de acidentes por mês</ChartTitle>
 
 					{loading ? (
-						<h1>Carregando</h1>
+						<div>
+							<img src={loader} alt="Carregando" />
+							<h1>Carregando</h1>
+						</div>
 					) : (
 						<ResponsiveContainer width={800} height="100%" maxHeight={600}>
 							<BarChart
@@ -94,49 +98,63 @@ function Statistics() {
 				<ChartWrapper>
 					<ChartTitle>Quantidade de acidentes por tipo</ChartTitle>
 
-					<ResponsiveContainer width={800} height="100%" maxHeight={600}>
-						<RadarChart cx="50%" cy="50%" outerRadius="80%" data={types}>
-							<PolarGrid />
-							<PolarAngleAxis dataKey="tipo" />
-							<PolarRadiusAxis />
-							<Radar
-								dataKey="count"
-								stroke="#00A588"
-								fill="#00A588"
-								fillOpacity={0.6}
-							/>
-						</RadarChart>
-					</ResponsiveContainer>
+					{loading ? (
+						<div>
+							<img src={loader} alt="Carregando" />
+							<h1>Carregando</h1>
+						</div>
+					) : (
+						<ResponsiveContainer width={800} height="100%" maxHeight={600}>
+							<RadarChart cx="50%" cy="50%" outerRadius="80%" data={types}>
+								<PolarGrid />
+								<PolarAngleAxis dataKey="tipo" />
+								<PolarRadiusAxis />
+								<Radar
+									dataKey="count"
+									stroke="#00A588"
+									fill="#00A588"
+									fillOpacity={0.6}
+								/>
+							</RadarChart>
+						</ResponsiveContainer>
+					)}
 				</ChartWrapper>
 
 				<ChartWrapper>
 					<ChartTitle>Quantidade de acidentes por bairro</ChartTitle>
 
-					<ResponsiveContainer width={800} height="100%" maxHeight={600}>
-						<LineChart
-							width={500}
-							height={300}
-							data={neighbourhoods}
-							margin={{
-								top: 5,
-								right: 30,
-								left: 20,
-								bottom: 5,
-							}}
-						>
-							<CartesianGrid strokeDasharray="3 3" />
-							<XAxis dataKey="bairro" />
-							<YAxis />
-							<Tooltip />
-							<Legend />
-							<Line
-								type="monotone"
-								dataKey="count"
-								stroke="#00A588"
-								activeDot={{ r: 8 }}
-							/>
-						</LineChart>
-					</ResponsiveContainer>
+					{loading ? (
+						<div>
+							<img src={loader} alt="Carregando" />
+							<h1>Carregando</h1>
+						</div>
+					) : (
+						<ResponsiveContainer width={800} height="100%" maxHeight={600}>
+							<LineChart
+								width={500}
+								height={300}
+								data={neighbourhoods}
+								margin={{
+									top: 5,
+									right: 30,
+									left: 20,
+									bottom: 5,
+								}}
+							>
+								<CartesianGrid strokeDasharray="3 3" />
+								<XAxis dataKey="bairro" />
+								<YAxis />
+								<Tooltip />
+								<Legend />
+								<Line
+									type="monotone"
+									dataKey="count"
+									stroke="#00A588"
+									activeDot={{ r: 8 }}
+								/>
+							</LineChart>
+						</ResponsiveContainer>
+					)}
 				</ChartWrapper>
 			</Container>
 		</Menu>
